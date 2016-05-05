0523dbeec40c7e14ad267d52a60e8f5ecce4454a
wxyBUPT <746451950@qq.com>
2016-05-05 14:38:58 +0800

    添加配置项

---
+++ conf_util.py
@@ -0,0 +1,81 @@
+#coding=utf-8
+__author__ = 'xiyuanbupt'
+import ConfigParser
+import os.path
+
+cf = ConfigParser.ConfigParser()
+cf.read('global.ini')
+
+class ConfUtil:
+
+    @classmethod
+    def getQTAudioDir(cls):
+        return cf.get('qingting','audio_dir')
+
+    @classmethod
+    def getKLAudioDir(cls):
+        return cf.get('kaola','audio_dir')
+
+    @classmethod
+    def getXMLYAudioDir(cls):
+        return cf.get('ximalaya','audio_dir')
+
+    @classmethod
+    def getMongoIP(cls):
+        return cf.get('mongo','ip')
+
+    @classmethod
+    def getMongoPort(cls):
+        return cf.getint('mongo','port')
+
+    @classmethod
+    def getStatisticsDBName(cls):
+        return cf.get('mongo','statistics_db')
+
+    @classmethod
+    def getSpiderDBName(cls):
+        return cf.get('mongo','spider_db')
+
+    @classmethod
+    def getCrontabCollectionName(cls):
+        return cf.get('mongo','crontab')
+
+    @classmethod
+    def getXMLYAlbumCollectionName(cls):
+        return cf.get('spider_collections','xmly_album')
+
+    @classmethod
+    def getXMLYCategoryCollectionName(cls):
+        return cf.get('spider_collections','xmly_category')
+
+    @classmethod
+    def getKLAlbumCollectionName(cls):
+        return cf.get('spider_collections','kl_album')
+
+    @classmethod
+    def getKLCategoryCollectionName(cls):
+        return cf.get('spider_collections','kl_category')
+
+    @classmethod
+    def getQTAlbumCollectionName(cls):
+        return cf.get('spider_collections','qt_item')
+
+    @classmethod
+    def getXMLYAudioCollectionName(cls):
+        '''
+        获得存储xmly 所有audio 信息的collection
+        :return:
+        '''
+        return cf.get('spider_collections','xmly_audio')
+
+    @classmethod
+    def getKLAudioCollectionName(cls):
+        return cf.get('spider_collections','kl_audio')
+
+    @classmethod
+    def getQTAudioCollectionName(cls):
+        return cf.get('spider_collections','qt_audio')
+
+    @classmethod
+    def getMediaSizeCollectionName(cls):
+        return cf.get('statistics_collections','media_size')
